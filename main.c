8c05bd63bc0985709bf1a7dae49db4296ce4dac7
Viktor-401 <victor.agostinho0123@gmail.com>
2025-02-18 03:37:52 -0300

    100%

--- main.c
+++ main.c
@@ -29,6 +29,7 @@ bool debounce();
 void button_press(uint gpio, uint32_t events); 
 void draw_display_border();
 void update_display();
+void setup_pwm(int pin);
 
 // Estado do led verde
 bool green_led = false;
@@ -40,6 +41,11 @@ int border_size = 1;
 absolute_time_t last_press = {0};
 
 ssd1306_t display;
+
+#define PERIOD 2000   // Período do PWM (valor máximo do contador)
+#define DIVIDER_PWM 16 // Divisor fracional do clock para o PWM
+int red_brightness = 0;
+int blue_brightness = 0;
 
 int main()
 {
@@ -67,9 +73,14 @@ int main()
     gpio_init(BLUE_LED_PIN);
     gpio_init(RED_LED_PIN);
 
+    // ADC
     adc_init();
     adc_gpio_init(JOYSTICK_X_PIN);
     adc_gpio_init(JOYSTICK_Y_PIN);
+
+    //PWM
+    setup_pwm(RED_LED_PIN);
+    setup_pwm(BLUE_LED_PIN);
 
     gpio_set_dir(BUTTON_A, GPIO_IN);
     gpio_set_dir(JOYSTICK_BUTTON, GPIO_IN);
@@ -89,16 +100,32 @@ int main()
     {
         adc_select_input(0);
         square_y = 32-(trunc((adc_read()-2030)/64));
-        printf("Y: %d\n", adc_read()); //2030 +- 10
-        printf("Coord y: %d\n", square_y);
+        blue_brightness = trunc(abs(adc_read()-2030));
+        blue_brightness = blue_brightness<40?0:blue_brightness;
+        printf("Y: %d\n", adc_read());
+        printf("Blue(0-2000): %d\n", blue_brightness);
 
         adc_select_input(1);
         square_x = 64+(trunc((adc_read()-2030)/32));
-        printf("X: %d\n", adc_read()); //1965 +- 10
-        printf("Coord x: %d\n", square_x);
+        red_brightness = trunc(abs(adc_read()-2030));
+        red_brightness = red_brightness<150?0:red_brightness;
+        printf("X: %d\n", adc_read());
+        printf("red(0-2000): %d\n", red_brightness);
 
         update_display();
-
+        if (activate_leds)
+        {
+            pwm_set_gpio_level(RED_LED_PIN, red_brightness);
+            pwm_set_gpio_level(BLUE_LED_PIN, blue_brightness);
+            gpio_put(GREEN_LED_PIN, green_led);
+        }
+        else
+        {
+            pwm_set_gpio_level(RED_LED_PIN, 0);
+            pwm_set_gpio_level(BLUE_LED_PIN, 0);
+            gpio_put(GREEN_LED_PIN, 0);
+        }
+        
         sleep_ms(10);
     }
 }
@@ -125,21 +152,13 @@ void button_press(uint gpio, uint32_t events)
         {
             case BUTTON_A:
                 activate_leds = !activate_leds;
-                if (activate_leds)
-                {
-                    gpio_init(GREEN_LED_PIN);
-                    gpio_init(BLUE_LED_PIN);
-                    gpio_init(RED_LED_PIN);
-                }
-                else
-                {
-                    gpio_deinit(GREEN_LED_PIN);
-                    gpio_deinit(BLUE_LED_PIN);
-                    gpio_deinit(RED_LED_PIN);
-                }
+                break;
             case JOYSTICK_BUTTON:
-                green_led = !green_led;
-                gpio_put(GREEN_LED_PIN, green_led);
+                if (activate_leds)
+                {                
+                    green_led = !green_led;
+                    gpio_put(GREEN_LED_PIN, green_led);
+                }
                 border_size += 2;
                 if (border_size > 7)
                 {
@@ -170,4 +189,15 @@ void draw_display_border()
     ssd1306_rect(&display, 64-border_size, 0, 128, border_size, true, true); // horizontal bottom
     ssd1306_rect(&display, 0, 0, border_size, 64, true, true); // vertical left
     ssd1306_rect(&display, 0, 128-border_size, border_size, 64, true, true); // vertical right
+}
+
+void setup_pwm(int pin)
+{
+    uint slice;
+    gpio_set_function(pin, GPIO_FUNC_PWM); // Configura o pino do pin para função PWM
+    slice = pwm_gpio_to_slice_num(pin);    // Obtém o slice do PWM associado ao pino do pin
+    pwm_set_clkdiv(slice, DIVIDER_PWM);    // Define o divisor de clock do PWM
+    pwm_set_wrap(slice, PERIOD);           // Configura o valor máximo do contador (período do PWM)
+    pwm_set_gpio_level(pin, 0);            // Define o nível inicial do PWM para o pino do LED
+    pwm_set_enabled(slice, true);          // Habilita o PWM no slice correspondente
 }
